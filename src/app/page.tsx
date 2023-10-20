e52f800278c22ad736e44ecefbcb163da1306c50
matheus <matheusb.dev@gmail.com>
2023-10-19 21:36:24 -0300

    chore: add next auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="bg-blue-500 font-bold text-red-500">
-        Hello World !
-      </div>
+      <div>Hello World!</div>
     </main>
   );
 }
