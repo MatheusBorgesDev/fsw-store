aa671d337582da867b726c8cb8ed58d87d691e1f
matheus <matheusb.dev@gmail.com>
2023-11-13 22:59:59 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 import Categories from "./components/Categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/ProductList";
@@ -8,29 +8,40 @@ export default async function Home() {
     where: {
       discountPercentage: {
         gt: 0,
-      }
-    }
-  })
+      },
+    },
+  });
 
   return (
     <main>
       <div>
         <Image
-           src="/banner-home-01.png"
-           height={0}
-           width={0}
-           className="h-auto w-full px-5"
-           sizes="100vw"
-           alt="Até 55% de desconto esse mês"
+          src="/banner-home-01.png"
+          height={0}
+          width={0}
+          className="h-auto w-full px-5"
+          sizes="100vw"
+          alt="Até 55% de desconto esse mês"
         />
+
         <div className="mt-8 px-5">
-          <Categories/>
+          <Categories />
         </div>
 
         <div className="mt-8 p-0">
-          <ProductList products={deals}/>
+          <p className="mb-3 ml-5 font-bold uppercase">Ofertas</p>
+          <ProductList products={deals} />
         </div>
       </div>
+
+      <Image
+          src="/banner-home-02.png"
+          height={0}
+          width={0}
+          className="h-auto w-full px-5"
+          sizes="100vw"
+          alt="Até 55% de desconto em Mouses"
+        />
     </main>
   );
 }
