1e840ea3da0fa1e3fae6576a56d99e29d6ebb8f0
matheus <matheusb.dev@gmail.com>
2023-12-27 18:02:34 -0300

    feat: add DiscountBadge

--- src/app/product/[slug]/components/ProductInfo.tsx
+++ src/app/product/[slug]/components/ProductInfo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge";
+import DiscountBadge from "@/components/ui/DiscountBadge";
 import { Button } from "@/components/ui/button";
 import { ProductWithTotalPrice } from "@/helpers/product";
 import {
@@ -38,9 +38,7 @@ const ProductInfo = ({
       <div className="flex items-center gap-2">
         <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
         {discountPercentage > 0 && (
-          <Badge className="px-2 py-[2px]">
-            <ArrowDownIcon size={14} /> {discountPercentage}% off
-          </Badge>
+          <DiscountBadge>{discountPercentage}</DiscountBadge>
         )}
       </div>
 
