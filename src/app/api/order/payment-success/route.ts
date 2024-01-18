3529c50d67dc4626fd4353fb2cc42fa3e4fb6dcf
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-01-18 20:41:45 -0300

    feat: ensure order is updated on checkout success

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -22,6 +22,8 @@ export const POST = async (request: Request) => {
   );
 
   if (event.type === "checkout.session.completed") {
+    const session = event.data.object as any;
+
     const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
       event.data.object.id,
       {
@@ -30,6 +32,14 @@ export const POST = async (request: Request) => {
     );
     const lineItems = sessionWithLineItems.line_items;
 
+    await prismaClient.order.update({
+      where: {
+        id: session.metadata.orderId,
+      },
+      data: {
+        status: "PAYMENT_CONFIRMED",
+      },
+    });
   }
 
   return NextResponse.json({ received: true });
