b014a6bcee88dfe27097f32252c67ded0b3385c3
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-01-23 19:42:58 -0300

    feat: add totals to OrderItem

--- src/app/orders/components/OrderItem.tsx
+++ src/app/orders/components/OrderItem.tsx
@@ -8,26 +8,46 @@ import { Card } from "@/components/ui/card";
 import { Prisma } from "@prisma/client";
 import { format } from "date-fns";
 import OrderProductItem from "./OrderProductItem";
+import { Separator } from "@/components/ui/separator";
+import { useMemo } from "react";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 interface OrderItemProps {
   order: Prisma.OrderGetPayload<{
     include: {
       orderProducts: {
-        include: {product: true}
-      }
+        include: { product: true };
+      };
     };
   }>;
 }
 
 const OrderItem = ({ order }: OrderItemProps) => {
+  const subtotal = useMemo(() => {
+    return order.orderProducts.reduce((acc, orderProduct) => {
+      return acc + Number(orderProduct.product.basePrice) * orderProduct.quantity;
+    }, 0)
+  }, [order.orderProducts])
+
+  const total = useMemo(() => {
+    return order.orderProducts.reduce((acc, product) => {
+      const productWithTotalPrice = computeProductTotalPrice(product.product)
+
+      return acc + Number(productWithTotalPrice.totalPrice) * product.quantity;
+    }, 0)
+  }, [order.orderProducts])
+
+  const totalDiscount = subtotal - total
+
   return (
     <Card className="px-5">
       <Accordion type="single" className="w-full" collapsible>
         <AccordionItem value={order.id}>
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
-              Pedido com {order.orderProducts.length}{" "}
-              {order.orderProducts.length !== 1 ? "produtos" : "produto"}
+              <p>Pedido com {order.orderProducts.length}{" "}
+              {order.orderProducts.length !== 1 ? "produtos" : "produto"}</p>
+              <span className="text-sm opacity-60">Feito em {format(order.createdAt, 'd/MM/yy')}</span>
             </div>
           </AccordionTrigger>
 
@@ -36,12 +56,14 @@ const OrderItem = ({ order }: OrderItemProps) => {
               <div className="flex items-center justify-between">
                 <div className="font-bold">
                   <span>Status</span>
-                  <p className="text-[#8162FF]">{order.status}</p>
+                  <p className="text-[#8162FF]">Aguardando pagamento</p>
                 </div>
 
                 <div>
                   <p className="font-bold">Data</p>
-                  <p className="opacity-60">{format(order.createdAt, "d/MM/yy")}</p>
+                  <p className="opacity-60">
+                    {format(order.createdAt, "d/MM/yy")}
+                  </p>
                 </div>
 
                 <div>
@@ -50,9 +72,36 @@ const OrderItem = ({ order }: OrderItemProps) => {
                 </div>
               </div>
 
-              {order.orderProducts.map(orderProduct => (
-                <OrderProductItem key={orderProduct.id} orderProduct={orderProduct} />
+              {order.orderProducts.map((orderProduct) => (
+                <OrderProductItem
+                  key={orderProduct.id}
+                  orderProduct={orderProduct}
+                />
               ))}
+
+              <div className="flex w-full flex-col gap-1 text-xs">
+                <Separator />
+
+                <div className="flex w-full justify-between py-3">
+                  <p>Subtotal</p>
+                  <p>R$ {subtotal.toFixed(2)}</p>
+                </div>
+
+                <div className="flex w-full justify-between py-3">
+                  <p>Entrega</p>
+                  <p>GRÁTIS</p>
+                </div>
+
+                <div className="flex w-full justify-between py-3">
+                  <p>Descontos</p>
+                  <p>- R$ {totalDiscount.toFixed(2)}</p>
+                </div>
+
+                <div className="flex w-full justify-between py-3 text-sm font-bold">
+                  <p>Total</p>
+                  <p>R$ {total.toFixed(2)}</p>
+                </div>
+              </div>
             </div>
           </AccordionContent>
         </AccordionItem>
